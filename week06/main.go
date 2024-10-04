b8fff3a4c03fffb90b2b1bab17e6f360ab211ce5
ssm0630so <ssm0630so@gmail.com>
2024-10-04 15:03:56 +0900

    v0.6 Unicode

--- week06/main.go
+++ week06/main.go
@@ -8,10 +8,14 @@ import (
 func main() {
 	var i int = 13
 	f := 12.9
+	c1 := 'Z' // 90
+	c2 := '김' // 44608, Unicode
+
 	fmt.Printf("value i : %d, vlue f : %f\n", i, f)
 	//fmt.Printf("%d * %f = %f\n", i, f, i*f)
 	//fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f) // conversion
-	fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
-	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
+	fmt.Printf("%d * %f = %d\n", i, f, i*int(f)) // conversion
+	fmt.Println(c1, c2)
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2))
 
 }
