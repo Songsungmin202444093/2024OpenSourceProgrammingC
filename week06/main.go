1132db6f3efce5bd9bc78b14a14fb16a81b291e0
ssm0630so <ssm0630so@gmail.com>
2024-10-04 15:48:52 +0900

    v0.7 Zero value

--- week06/main.go
+++ week06/main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 ) // c언어의 #include <stdio.h> 역할
 
 func main() {
-	var i int = 13
-	f := 12.9
-	c1 := 'Z' // 90
-	c2 := '김' // 44608, Unicode
+	var f float64
+	var i int
+	var b bool
+	var s string
 
-	fmt.Printf("value i : %d, vlue f : %f\n", i, f)
-	//fmt.Printf("%d * %f = %f\n", i, f, i*f)
-	//fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f) // conversion
-	fmt.Printf("%d * %f = %d\n", i, f, i*int(f)) // conversion
-	fmt.Println(c1, c2)
-	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2))
+	mySchoolAccount := s
+	println(mySchoolAccount)
+
+	fmt.Println(f, b, s, i)
+	fmt.Printf("%f%t%s%d\n", f, b, s, i) // zero value
+	f = 2.7
+	i = 3
+	fmt.Print("\n\n", f <= float64(i), "\n") // comparison (true/false)
 
 }
