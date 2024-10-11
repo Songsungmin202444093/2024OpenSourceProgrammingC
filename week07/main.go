ffd79565d087b975619c509e888f55b5f4d91616
ssm0630so <ssm0630so@gmail.com>
2024-10-11 15:27:31 +0900

    v1.3 Parse

--- week07/main.go
+++ week07/main.go
@@ -5,17 +5,28 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 // c언어의 #include <stdio.h> 역할
 
 func main() {
+	fmt.Print("Input your score : ")
 	in := bufio.NewReader(os.Stdin)
-	fmt.Print("Input your name : ")
-	name, err := in.ReadString('\n')
+	i, err := in.ReadString('\n')
+
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		fmt.Println(name)
 	}
+	i = strings.TrimSpace(i)                // 줄바꿈등 제거. 파이썬의 strip 함수와 비슷
+	score, _ := strconv.ParseInt(i, 16, 32) // 16진수 정수형(32bit)으로 변환
+	if score >= 60 {
+		fmt.Println("A")
+		fmt.Printf("%d\n", score)
+	} else {
+		fmt.Println("BCDF")
+		fmt.Printf("%d\n", score)
+	}
+
 }
