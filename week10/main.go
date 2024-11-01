2c65cd86a129f047411d4393f14ce972198c9fac
ssm0630so <ssm0630so@gmail.com>
2024-11-01 15:17:14 +0900

    v2.6 Prime number

--- week10/main.go
+++ week10/main.go
@@ -4,12 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func main() {
+	//fmt.Printf("%f\n", math.Sqrt(19.0))
 	fmt.Print("Input number : ")
 	in := bufio.NewReader(os.Stdin)
 	i, err := in.ReadString('\n')
@@ -29,7 +31,7 @@ func main() {
 		isPrime = false
 	} else {
 		j := 2
-		for j < n {
+		for j <= int(math.Sqrt(float64(n))) {
 			if n%j == 0 {
 				isPrime = false // 더하기 연산 제거
 				break           // performance up
