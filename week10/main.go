fe199991ad4f6645b4c855b0b6a78097c528572d
ssm0630so <ssm0630so@gmail.com>
2024-11-01 14:26:55 +0900

    v2.4 Prime number

--- week10/main.go
+++ week10/main.go
@@ -23,18 +23,21 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//counts := 0
 	var isPrime bool = true // 가독성 up, 메모리 down
-	j := 2
-	for j < n {
-		if n%j == 0 {
-			//counts++
-			isPrime = false // 더하기 연산 제거
+
+	if n <= 1 {
+		isPrime = false
+	} else {
+		j := 2
+		for j < n {
+			if n%j == 0 {
+				//counts++
+				isPrime = false // 더하기 연산 제거
+			}
+			j++
 		}
-		j++
 	}
 
-	//if counts == 0 {
 	if isPrime { // == 비교 연산 제거
 		fmt.Printf("%d is prime number.", n)
 	} else {
