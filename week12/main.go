5e7d5b11f767c242709b040c6b7018d0f2ad066b
ssm0630so <ssm0630so@gmail.com>
2024-11-15 15:50:02 +0900

    v4.0 Slice

--- week12/main.go
+++ week12/main.go
@@ -1,18 +1,12 @@
 package main
 
-import (
+import {
 	"fmt"
-
-	"github.com/headfirstgo/keyboard"
-)
+}
 
 func main() {
-	var gpas [3]float64
-	for i := 0; i < len(gpas); i++ {
-		fmt.Print("input float number : ")
-		gpas[i], _ = keyboard.GetFloat()
-	}
-	for _, gpa := range gpas {
-		fmt.Println(gpa)
-	}
+	var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 3.9, 4.23}
+	gpa_slice := pa[1:4] 
+	fmt.Println(gpa, reflect.TypeOf(gpa))
+	fmt.Println(gpa_slice, reflect.TypeOf(gpa_slice))
 }
