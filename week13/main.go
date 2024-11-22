a4261cd89e1b387347afa43689b0939aa64d4091
ssm0630so <ssm0630so@gmail.com>
2024-11-22 14:27:24 +0900

    v4.1 Slice

--- week13/main.go
+++ week13/main.go
@@ -5,7 +5,11 @@ import (
 )
 
 func main() {
+	var emptySlice []bool
+	emptySlice = make([]bool, 5)
+	fmt.Printf("%#v\n", emptySlice)
 	var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 3.9, 4.23}
+
 	gpa_slice := gpa[1:4]
 	gpa[2] = 2.71
 	gpa_slice = append(gpa_slice, 4.3, 5.55)
